c53d80ca0832941994363f632b3267e6945b7d4e
Marc Poppy <poppy.marc@gmail.com>
2019-05-26 00:13:00 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -143,6 +143,7 @@ def all_holidays_with_bbq(holiday_hash)
   holiday_hash.each do |season, holiday_data|
     holiday_data.select do |holiday, supplies|
       if supplies == "BBQ"
+        bbq_holidays = []
         bbq_holidays << holiday
       end
     end
